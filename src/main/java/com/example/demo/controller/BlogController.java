0aae63a46c178d8869c1de34e496fd51fbe3d2c1
kimchaeyeonn <betty6994@naver.com>
2024-10-30 23:17:10 +0900

    6주차 - 블로그 게시판 - 2

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -1,11 +1,17 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import com.example.demo.model.domain.Article;
 import com.example.demo.model.service.BlogService;
 import com.example.demo.model.service.AddArticleRequest;
@@ -33,6 +39,34 @@ public class BlogController {
     @PostMapping("/api/articles")
     public String addArticle(@ModelAttribute AddArticleRequest request) {
         blogService.save(request);
+        return "redirect:/article_list";
+    }
+
+    @GetMapping("/article_edit/{id}") // 게시판 링크 지정
+    public String article_edit(Model model, @PathVariable Long id) {
+        Optional<Article> list = blogService.findById(id); // 선택한 게시판 글
+        List<Article> articles = blogService.findAll(); // 모든 게시글 조회
+
+        if (list.isPresent()) {
+            model.addAttribute("article", list.get()); // 존재하면 Article 객체를 모델에 추가
+            model.addAttribute("articles", articles); // 모든 게시글 목록을 모델에 추가
+        } else {
+            // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
+            return "/error_page/article_error"; // 오류 처리 페이지로 연결(이름 수정됨)
+        }
+        return "article_edit"; // .HTML 연결
+    }
+
+
+    @PutMapping("/api/article_edit/{id}")
+    public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
+        blogService.update(id, request);
+        return "redirect:/article_list"; // 글 수정 이후 .html 연결
+    }
+
+    @DeleteMapping("/api/article_delete/{id}")
+    public String deleteArticle(@PathVariable Long id) {
+        blogService.delete(id);
         return "redirect:/article_list";
     }
 }
